c22f0aefd4a4b183552ae36d9a9aaaa87f86617e
whoisanetta <122131174+whoisanetta@users.noreply.github.com>
2023-09-07 10:32:02 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryLi = galleryItems
+  .map(({ original, preview, description }) => {
+    return `
+    <li class="gallery__item ">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image" 
+                src="${preview}"
+                
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryLi);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
